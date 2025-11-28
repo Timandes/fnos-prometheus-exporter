babdb3b5e24aa20a49e5839e544f45e83069dbbd
隆宇 <yanan.yn@alibaba-inc.com>
2025-11-28 09:36:33 +0800

    chore: bump versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fnos-exporter",
-    version="0.4.0",
+    version="0.5.0",
     packages=find_packages(),
     py_modules=['main'],
     install_requires=[
